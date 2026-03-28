5d28937e24a6ea4fcb83b92e3c4e409a03e5c7a3
sohum-arora <sohumarora28@gmail.com>
2026-03-28 10:27:50 +0530

    RESOLVED CONFLICTS

--- TeamCode/src/main/java/Drivetrains/Drivetrain.java
+++ TeamCode/src/main/java/Drivetrains/Drivetrain.java
@@ -1,17 +1,7 @@
 package Drivetrains;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
-=======
->>>>>>> 5e9d75400a1896ae8936189bff63bffa3688d89b
-=======
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-
->>>>>>> be745d4b69d7d4ab5db5f97c7cc37107e354a4af
 
 import java.util.List;
 
@@ -21,27 +11,15 @@ import java.util.List;
  */
 public abstract class Drivetrain {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //set power methods
-    public abstract void setPower(DcMotorEx motor,double power);
-    public abstract void setPower(List<DcMotorEx> motors, double power);
-=======
+
     //set power methods
     public abstract void setPower(DcMotorEx motor,double power);
     public abstract void setPower(List<DcMotorEx> motors, double power);
 
->>>>>>> be745d4b69d7d4ab5db5f97c7cc37107e354a4af
+    //set power methods
     public abstract void setPower(double power);
     //drive train init method
     public abstract void initDrive(HardwareMap hardwareMap, String lfName, String rfName, String lrName, String rrName);
 
-<<<<<<< HEAD
-=======
-    public abstract void setPower(DcMotorEx motor,double power);
-    public abstract void setPower(List<DcMotorEx> motors, double power);
->>>>>>> 5e9d75400a1896ae8936189bff63bffa3688d89b
-=======
 
->>>>>>> be745d4b69d7d4ab5db5f97c7cc37107e354a4af
 }
